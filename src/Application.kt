aeaf90db81c663b356450f8081e70171212de2ac
Chris Athanas <realityexpander@gmail.com>
2022-06-26 04:58:01 -0500

    Add deployment comments

--- src/Application.kt
+++ src/Application.kt
@@ -20,6 +20,104 @@ package com.realityexpander
 //   -> enter password: password    <-- privateKeyPassword
 //   -> enter password again: password
 
+// Deploy to an Ubuntu 20.04 (Focal) server:
+//
+// from root of project, run "gradle jar":
+//   ./gradle jar
+// Run sftp to upload the jar of the app to the server:
+//   sftp root@<ip address of server>:/home
+//   --> enter password
+//   put app-0.0.1.jar
+// Make a new tab in the terminal, to upload the keys to the server:
+//   cd keys
+//   sftp root@<ip address of server>:/home/keys
+//   --> enter password
+//   put ktornoteapp.jks
+//
+// Start the server:
+// Make a new tab in the terminal.
+//   ssh root@<ip address of server>
+//   --> enter password
+//   sudo apt-get update
+//   cd /home
+//   ls
+// Should see our files
+//   sudo apt-get install openjdk-8-jdk
+//   --> Press enter to install java
+//   java -version
+// Install community edition of MongoDB:
+// https://www.mongodb.com/docs/manual/tutorial/install-mongodb-on-ubuntu/
+//   // wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
+//   wget -qO - https://www.mongodb.org/static/pgp/server-5.0.asc | sudo apt-key add -
+// Reload the package database:
+//   sudo apt-get update
+// Install the MongoDB packages:
+//   sudo apt-get install -y mongodb-org
+// Start the MongoDB service:
+//   sudo systemctl start mongod
+//   sudo systemctl status mongod
+// Install nano editor:
+//   sudo apt-get install -y nano
+// Create service configuration file for our app:
+//   sudo nano /etc/systemd/system/ktornoteapp.service
+//
+//   [Unit]
+//   Description=Ktor Note App service
+//   After=mongod.service              // defines which service to start after
+//   StartLimitIntervalSec=0           // prevents the service from not being started if it fails to start more than 5 times
+//
+//   [Service]
+//   Type=simple
+//   RestartSec=5                      // how long to wait before restarting the service
+//   Restart=always                    // restart the service if it fails to start
+//   User=root                         // run the service as root user
+//   // ExecStart=/home/ktornoteapp.sh // good shortcut for absolute path (** -> dont add this commented line)
+//   ExecStart=/usr/bin/java -jar /home/app-0.0.1.jar
+//
+//   [Install]
+//   WantedBy=multi-user.target
+//   ^s^x                              // Save and Exit
+// Start the service:
+//   sudo systemctl start ktornoteapp  // start the service
+//   sudo systemctl status ktornoteapp // check if the service is running & show logs, 'q' to quit
+//   sudo systemctl enable ktornoteapp // enable the service to start at boot time
+
+// Show the logs:
+//   journalctl -u ktornoteapp.service // -f to show logs from the last 24 hours, 'q' to quit
+//   journalctl -u ktornoteapp.service | grep -i error // grep for errors
+//   journalctl -u ktornoteapp.service | grep -i error | tail -n 10 // show the last 10 errors
+//   journalctl -u ktornoteapp.service | grep -i error | tail -n 10 | less // show the last 10 errors in a less window
+//   journalctl -u ktornoteapp.service | grep -i error | tail -n 10 | less -R // show the last 10 errors in a less window, with readline support
+//   journalctl -u ktornoteapp.service | grep -i error | tail -n 10 | less -R -S // show the last 10 errors in a less window, with readline support and scrollback
+//   journalctl -u ktornoteapp.service | grep -i error | tail -n 10 | less -R -S -X // show the last 10 errors in a less window, with readline support and scrollback and exit on 'q'
+//   journalctl -u ktornoteapp.service | grep -i error | tail -n 10 | less -R -S -X -M // show the last 10 errors in a less window, with readline support and scrollback and exit on 'q' and show the cursor at the end of the file
+//   journalctl -u ktornoteapp.service | grep -i error | tail -n 10 | less -R -S -X -M -F // show the last 10 errors in a less window, with readline support and scrollback and exit on 'q' and show the cursor at the end of the file and follow the logs
+//   journalctl -u ktornoteapp.service | grep -i error | tail -n 10 | less -R -S -X -M -F -N // show the last 10 errors in a less window, with readline support and scrollback and exit on 'q' and show the cursor at the end of the file and follow the logs and dont show the cursor
+
+// *note: May need to restart the app service after these commands.
+// Disable firewall:
+//   sudo apt-get install ufw
+// Get status of firewall:
+//   sudo ufw status
+// Disable firewall:
+//   sudo ufw disable
+// Enable firewall:
+//   sudo ufw enable
+// Allow incoming connections:
+//   sudo ufw allow <port>    // 8002 for our app
+// Allow outgoing connections:
+//   sudo ufw allow out <port>
+// Allow incoming connections from a specific IP:
+//   sudo ufw allow from <ip>
+// Allow outgoing connections to a specific IP:
+//   sudo ufw allow out from <ip>
+// Allow incoming connections from a specific IP and port:
+//   sudo ufw allow from <ip> port <port>
+// Allow outgoing connections to a specific IP and port:
+//   sudo ufw allow out to <ip> port <port>
+
+
+
 import ch.qos.logback.classic.Level
 import ch.qos.logback.classic.LoggerContext
 import com.realityexpander.data.checkPasswordForEmail
